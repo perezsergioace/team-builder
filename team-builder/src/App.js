f0b116a060745113a10c92aae856c0f746284402
perezsergioace <perez.sergio.ace@gmail.com>
2019-11-13 23:11:51 -0500

    MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,37 +1,3 @@
-// import React, { useState } from 'react';
-
-// import Form from "./components/Form";
-// import Card from "./components/Card";
-
-
-// import './App.css';
-
-// function App() {
-//   const [teamMember, setTeamMember] = useState([
-//     {
-//       name: "Sergio Perez",
-//       email: "TeamMember@lambda.com",
-//       role: "Full Stack Student"
-//     }  
-//   ]);
-
-//   const addNewMember = member => {
-//     const newTeamMember = setTeamMember([...teamMember, member]);
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>Team Builder</h1>
-//       <Form addNewMember={addNewMember}/>
-//       <Card teamMember={teamMember}/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import React, { useState } from 'react';
 
 import './App.css';
